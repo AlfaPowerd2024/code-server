ac21cb9cfc4b7b26e617d05b07c3549b9f2f24a7
AlfaPowerd2024 <145537524+AlfaPowerd2024@users.noreply.github.com>
2024-09-01 04:38:18 +0000

    Changes to be committed: 	modified:   inew.py

--- inew.py
+++ inew.py
@@ -1,1 +1,12 @@
 print("A")
+ans = int(input(""))
+if ans == 1:
+    print("One  ")
+elif ans == 2:
+    print("Two  ")
+elif ans == 3:
+    print("Three    ")
+elif ans == 4:
+    print("Four  ")
+else:
+    print("Not All of Them")

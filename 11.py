f1f58dcc4faf6ef5dca21d20bca479461d4f2922
AlfaPowerd2024 <145537524+AlfaPowerd2024@users.noreply.github.com>
2024-09-01 04:01:52 +0000

    Added 11.py and 11.5.py

---
+++ 11.py
@@ -0,0 +1,18 @@
+print(" Main Menu :  ")
+print("  1: Start App  ")
+print("  2: Quit App  ")
+KGP = int(input("Enter Your Menu Select Between 1 - 2  __##  "))
+if KGP == 2:
+    print("Goodbye, Have a Nice Day!")
+    quit()
+    print("Quit Work Test")
+if KGP == 1:
+    print("Welcome To App :) ")
+if KGP == 1:
+    print("***ﺪﺷﺎﺒﯿﻣ ٩٤ ﻝﺎﺳ ﺖﻤﯿﻗ ﻪﺑ ﺎﻫ ﺖﻤﯿﻗ***")
+    print("12,000 : ﻮﻠﭘ ﺎﯿﺑﻮﻟ -١")
+    print("14,000 : ﻮﻠﭘ ﻍﺮﻣ -٢")
+    print("18,000  : ﯽﻫﺎﻣ -٣")
+    print("2,000   : ﺖﻠﻣﺍ -٤")
+    ans = int(input("ﺪﯿﻨﻛ ﻝﺎﺳﺭﺍ ﺍﺭ ٩ ﺩﺪﻋ ﺕﺭﻮﺼﻨﯾﺍ ﺮﯿﻏﺭﺩ ﻭ ﻩﺩﺮﻛ ﺩﺭﺍﻭ ﺍﺭﺩﻮﺧ ﻮﻨﻣ ﺮﻈﻧ ﺩﺭﻮﻣ ﻩﺭﺎﻤﺷ ﺎﻔﻄﻟ  __###  "))
+    

3c299063658093358dbd82dea195521b90e55648
AlfaPowerd2024 <145537524+AlfaPowerd2024@users.noreply.github.com>
2024-09-08 14:54:41 +0000

    new file:   neweeed.py

---
+++ neweeed.py
@@ -0,0 +1,11 @@
+c165 = "---------"
+max = 0
+def maxim():
+    nym = int(input("Enter a number: "))
+    nun = int(input("Enter a number: "))
+    if nym > nun :
+        nun = nym
+    else :
+        max = nun
+    print(max)
+maxim()

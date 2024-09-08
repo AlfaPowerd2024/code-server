d719d131093dcbcf241fa31a2a4760c85239157a
AlfaPowerd2024 <145537524+AlfaPowerd2024@users.noreply.github.com>
2024-09-08 15:12:18 +0000

    new file:   newwoof.py

---
+++ newwoof.py
@@ -0,0 +1,6 @@
+def myfunc(x):
+    return x*x*x
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+c = int(input("Enter another number: "))
+print(myfunc(a) + myfunc(b) + myfunc(c))
